a7fb450ab7f3af2f97137f420805a4d14295888a
neilkrichi <neilkrichi@gmail.com>
2018-03-23 20:41:45 -0700

    added search bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,59 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailError: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
+
+  }
+
+  handleInputChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  validateEmail(){
+    if (this.state.email === '') {
+      this.setState({emailError: ''})
+    }
+    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,9}$/i.test(this.state.email)) {
+      this.setState({emailError: 'error'})
+    }else{
+      this.setState({emailError: 'nice'})
+    }
+  }
+
   render() {
     return (
       <div className='app'>
         <header className='app-header'>
           <h1 className='app-title'>Invoice Simple Coding Challenge</h1>
         </header>
+
+        <div className='signup'>
+          <div className='page-container'>
+            <form onSubmit={this.handleSubmit}>
+              <div className={this.state.emailError}>
+                <label>Email </label><br/>
+                <input type ='text' name='email'
+                  onBlur={this.validateEmail}
+                  value={this.state.email}
+                  placeholder="example@email.com"
+                  onChange={this.handleInputChange}/>
+              </div>
+              <input type="submit" onClick={this.handleSubmit} />
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
